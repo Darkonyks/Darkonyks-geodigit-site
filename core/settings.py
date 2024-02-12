2493cb7645f394d57d668702a3d8859819b2afe5
Darko Nedic <darko.nedic83@gmail.com>
2024-02-12 12:57:45 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -126,11 +126,12 @@ LANGUAGES = (
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale/')]
 
-STATIC_URL = 'static/'
+STATIC_ROOT = (BASE_DIR / 'static')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'Gsite/static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Gsite/static'),
+]
+
 STATIC_ROOT = [os.path.join(BASE_DIR, 'Gsite/static')]
 
 MEDIA_ROOT = BASE_DIR / 'media'
