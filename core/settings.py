585f9980f797707072ce76da04aead86b4b07811
Darko Nedic <darko.nedic83@gmail.com>
2024-02-14 08:30:50 +0100

    reCAPTCHA

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost', 'ondigitalocean.app').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1, localhost').split(',')
 
 
 # Application definition
