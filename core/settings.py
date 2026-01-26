b34e776fecb29ea523ee05543bd4713ee28d8444
Darko Nedic <nedic.darko@geodigit.rs>
2026-01-26 10:41:53 +0100

    Update STATIC_ROOT path to use 'staticfiles' directory

--- core/settings.py
+++ core/settings.py
@@ -175,7 +175,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale/')]
 STATIC_URL = 'static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'Gsite/', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
